c98c7f2fcf9cf782674dbfe9638a57b8c8f598af
kunal.gangaher <kunal.gangaher@servicenow.com>
2023-11-07 17:51:46 +0530

    STRY56331515: Changing the place of logging debug log response.

--- dist/index.js
+++ dist/index.js
@@ -5228,15 +5228,15 @@ function circularSafeStringify(obj) {
         result = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
     } catch (e) {
         core.debug('[ServiceNow DevOps] Register Sonar Scan Summaries, Error: '+JSON.stringify(e));
+        if(e.response && e.response.data) {
+            var responseObject=circularSafeStringify(e.response.data);
+            core.debug('[ServiceNow DevOps] Register Sonar Scan Summaries, Status code :'+e.response.statusCode+', Response data :'+responseObject);          
+        }
+
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
             core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
         } else if (e.message.includes('401')) {
             core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
-            if(e.response && e.response.data) 
-            {
-                var responseObject=circularSafeStringify(e.response.data);
-                core.debug('[ServiceNow DevOps] Register Sonar Scan Summaries, Response data :'+responseObject);          
-            }
         } else if(e.message.includes('400') || e.message.includes('404')){
             let errMsg = '[ServiceNow DevOps] Register Sonar Scan Summaries are not Successful. ';
             let errMsgSuffix = ' Please provide valid inputs.';

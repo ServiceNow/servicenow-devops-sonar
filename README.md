83348f2224748916b4cb5d7bf422a33f37ce7d47
deepak.janmanchi <deepak.janmanchi@servicenow.com>
2024-01-30 15:33:47 +0530

    STRY55387793: secret authetication changes on dev branch

--- README.md
+++ README.md
@@ -4,33 +4,56 @@ This custom action needs to be added at step level in a job to register sonar de
 
 # Usage
 ## Step 1: Prepare values for setting up your secrets for Actions
-- credentials (username and password for a ServiceNow devops integration user)
+- credentials (Devops integration token of a GitHub tool created in ServiceNow DevOps or username and password for a ServiceNow devops integration user)
 - instance URL for your ServiceNow dev, test, prod, etc. environments
 - tool_id of your GitHub tool created in ServiceNow DevOps
 - sonar URL for your SonarQube instance or Sonar Cloud, for example **https://sonarcloud.io**
-- sonar organization the key for your organization in Sonar instance, for example **devops**
 - sonar project the key for your project in Sonar instance, for example **org.examples:demo**
 
 ## Step 2: Configure Secrets in your GitHub Ogranization or GitHub repository
 On GitHub, go in your organization settings or repository settings, click on the _Secrets > Actions_ and create a new secret.
 
-Create secrets called 
-- `SN_DEVOPS_USER`
-- `SN_DEVOPS_PASSWORD`
+For token based authentication which is available from @v2.0 , create secrets called 
 - `SN_INSTANCE_URL` your ServiceNow instance URL, for example **https://test.service-now.com**
 - `SN_ORCHESTRATION_TOOL_ID` only the **sys_id** is required for the GitHub tool created in your ServiceNow instance
+- `SN_DEVOPS_INTEGRATION_TOKEN` required for token based authentication
 - `SONAR_URL` the URL of your Sonar instance, for example **https://sonarcloud.io**
-- `SONAR_PROJECT_KEY` the project key in your Sonar instance, for example **org.examples:demo**
-- `SONAR_ORG_KEY` the project key in your Sonar instance, for example **devops**
+- `SONAR_PROJECT_KEY` the project key in your Sonar instance, for example 
+
+For basic authentication, create secrets called 
+- `SN_INSTANCE_URL` your ServiceNow instance URL, for example **https://test.service-now.com**
+- `SN_ORCHESTRATION_TOOL_ID` only the **sys_id** is required for the GitHub tool created in your ServiceNow instance
+- `SN_DEVOPS_USER`
+- `SN_DEVOPS_PASSWORD`
+- `SONAR_URL` the URL of your Sonar instance, for example **https://sonarcloud.io**
+- `SONAR_PROJECT_KEY` the project key in your Sonar instance, for example 
 
 ## Step 3: Configure the GitHub Action if need to adapt for your needs or workflows
+## For Token based Authentication which is available from @v2.0 , at ServiceNow instance
 ```yaml
 build:
     name: Build
     runs-on: ubuntu-latest
     steps:
       - name: ServiceNow DevOps Sonar Scan Results
-        uses: ServiceNow/servicenow-devops-sonar@v1
+        uses: ServiceNow/servicenow-devops-sonar@v2.0
+        with:
+          devops-integration-token: ${{ secrets.SN_DEVOPS_INTEGRATION_TOKEN}}
+          instance-url: ${{ secrets.SN_INSTANCE_URL }}
+          tool-id: ${{ secrets.SN_ORCHESTRATION_TOOL_ID }}
+          context-github: ${{ toJSON(github) }}
+          job-name: 'Build'
+          sonar-host-url: ${{ secrets.SONAR_URL }}
+          sonar-project-key: ${{ secrets.SONAR_PROJECT_KEY }}
+```
+## For Basic Authentication at ServiceNow instance
+```yaml
+build:
+    name: Build
+    runs-on: ubuntu-latest
+    steps:
+      - name: ServiceNow DevOps Sonar Scan Results
+        uses: ServiceNow/servicenow-devops-sonar@v1.34.2
         with:
           devops-integration-user-name: ${{ secrets.SN_DEVOPS_USER }}
           devops-integration-user-password: ${{ secrets.SN_DEVOPS_PASSWORD }}
@@ -40,7 +63,6 @@ build:
           job-name: 'Build'
           sonar-host-url: ${{ secrets.SONAR_URL }}
           sonar-project-key: ${{ secrets.SONAR_PROJECT_KEY }}
-          sonar-org-key: ${{ secrets.SONAR_ORG_KEY }}
 ```
 The values for secrets should be setup in Step 1. Secrets should be created in Step 2. The Step Name should be **ServiceNow DevOps Sonar Scan Results**.
 
@@ -48,11 +70,15 @@ The values for secrets should be setup in Step 1. Secrets should be created in S
 
 ### `devops-integration-user-name`
 
-**Required**  DevOps Integration Username to ServiceNow instance. 
+**Optional**  DevOps Integration Username to ServiceNow instance for basic authentication.
 
 ### `devops-integration-user-password`
 
-**Required**  DevOps Integration User Password to ServiceNow instance. 
+**Optional**  DevOps Integration User Password to ServiceNow instance for basic authentication. 
+
+### `devops-integration-token`
+
+**Optional**  DevOps Security Token of GitHub tool created in ServiceNow instance for token based authentication. 
 
 ### `instance-url`
 
@@ -68,7 +94,7 @@ The values for secrets should be setup in Step 1. Secrets should be created in S
 
 ### `job-name`
 
-**Required**  Display name of the job given for attribute _name_ in which _steps_ have been added for ServiceNow sonar custom action.
+**Required**  Display name of the job given for attribute _name_ in which _steps_ have been added for this custom action. For example, if display name of job is _Build_ then job-name value must be _'Build'_
 
 ### `sonar-host-url`
 
@@ -77,10 +103,6 @@ The values for secrets should be setup in Step 1. Secrets should be created in S
 ### `sonar-project-key`
 
 **Required**  The project key in your Sonar instance URL.
-
-### `sonar-org-key`
-
-The organization key in your Sonar instance URL. This is required only when your scan summaries available in Sonar Cloud.
 
 ## Outputs
 No outputs produced.
--- src/main.js
+++ src/main.js
@@ -1,29 +1,16 @@
 const core = require('@actions/core');
 const axios = require('axios');
 
-function circularSafeStringify(obj) {
-    const seen = new WeakSet();
-    return JSON.stringify(obj, (key, value) => {
-        if (key === '_sessionCache') return undefined;
-        if (typeof value === 'object' && value !== null) {
-        if (seen.has(value)) {
-          return '[Circular]';
-        }
-        seen.add(value);
-      }
-      return value;
-    });
-}
 
 (async function main() {
-    const instanceUrl = core.getInput('instance-url', { required: true });
+    let instanceUrl = core.getInput('instance-url', { required: true });
     const toolId = core.getInput('tool-id', { required: true });
-    const username = core.getInput('devops-integration-user-name', { required: true });
-    const password = core.getInput('devops-integration-user-password', { required: true });
+    const username = core.getInput('devops-integration-user-name', { required: false });
+    const password = core.getInput('devops-integration-user-password', { required: false });
+    const devopsIntegrationToken = core.getInput('devops-integration-token', { required: false });
     const jobname = core.getInput('job-name', { required: true });
     const projectKey = core.getInput('sonar-project-key', { required: true });
-    const orgKey = core.getInput('sonar-org-key', { required: false });
-    const sonarUrl = core.getInput('sonar-host-url', { required: true });
+    let sonarUrl = core.getInput('sonar-host-url', { required: true });
 
     let githubContext = core.getInput('context-github', { required: true });
 
@@ -32,12 +19,18 @@ function circularSafeStringify(obj) {
     } catch (e) {
         core.setFailed(`Exception parsing github context ${e}`);
     }
-
-    const endpoint = `${instanceUrl}/api/sn_devops/devops/tool/softwarequality?toolId=${toolId}`;
-
+            
     let payload;
     
     try {
+        sonarUrl = sonarUrl.trim();
+        if (sonarUrl.endsWith('/'))
+            sonarUrl = sonarUrl.slice(0, -1);
+
+        instanceUrl = instanceUrl.trim();
+        if (instanceUrl.endsWith('/'))
+            instanceUrl = instanceUrl.slice(0, -1);
+
         payload = {
             toolId: toolId,
             runId: `${githubContext.run_id}`,
@@ -47,65 +40,63 @@ function circularSafeStringify(obj) {
             sha: `${githubContext.sha}`,
             workflow: `${githubContext.workflow}`,
             projectKey: `${projectKey}`,
-            orgKey: `${orgKey}`,
             sonarUrl: `${sonarUrl}`,
             repository: `${githubContext.repository}`,
             ref: `${githubContext.ref}`,
             refName: `${githubContext.ref_name}`,
             refType: `${githubContext.ref_type}`
         };
-        core.debug(`Sonar Custom Action payload is : ${JSON.stringify(payload)}\n\n`);
+        core.debug('Sonar Custon Action payload is : ${JSON.stringify(payload)}\n\n');
     } catch (e) {
         core.setFailed(`Exception setting the payload ${e}`);
         return;
     }
 
-    let result;
+    const endpointv1 = `${instanceUrl}/api/sn_devops/v1/devops/tool/softwarequality?toolId=${toolId}`;
+    const endpointv2 = `${instanceUrl}/api/sn_devops/v2/devops/tool/softwarequality?toolId=${toolId}`;
+    let endpoint;
+    let httpHeaders;
+    let snowResponse;
 
     try {
-        const token = `${username}:${password}`;
-        const encodedToken = Buffer.from(token).toString('base64');
-
-        const defaultHeaders = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': 'Basic ' + `${encodedToken}`
-        };
-
-        let httpHeaders = { headers: defaultHeaders };
-        core.debug("[ServiceNow DevOps], Sending Request for Sonar, Request Header :"+JSON.stringify(httpHeaders)+", Payload :"+JSON.stringify(payload)+"\n");
-        result = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
-        core.debug("[ServiceNow DevOps], Receiving response for Sonar, Response :"+circularSafeStringify(result)+"\n");
-    } catch (e) {
-        core.debug('[ServiceNow DevOps] Register Sonar Scan Summaries, Error: '+JSON.stringify(e)+"\n");
-        if(e.response && e.response.data) {
-            var responseObject=circularSafeStringify(e.response.data);
-            core.debug('[ServiceNow DevOps] Register Sonar Scan Summaries, Status code :'+e.response.status+', Response data :'+responseObject+"\n");          
+        if (!devopsIntegrationToken && !username && !password) {
+            core.setFailed('Either secret token or integration username, password is needed for integration user authentication');
+            return;
+        } else if (devopsIntegrationToken) {
+            const defaultHeadersv2 = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'sn_devops_token': `${devopsIntegrationToken}`
+            };
+            httpHeaders = {
+                headers: defaultHeadersv2
+            };
+            endpoint = endpointv2;
+        } else if (username && password) {
+            const token = `${username}:${password}`;
+            const encodedToken = Buffer.from(token).toString('base64');
+            const defaultHeadersv1 = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Basic ' + `${encodedToken}`
+            };
+            httpHeaders = {
+                headers: defaultHeadersv1 
+            };
+            endpoint = endpointv1;
+        } else {
+            core.setFailed('For Basic Auth, Username and Password is mandatory for integration user authentication');
+            return;
         }
-
+        snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
+    } catch (e) {
         if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
             core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
         } else if (e.message.includes('401')) {
-            core.setFailed('Invalid username and password or Invalid token and toolid. Please correct the input parameters and try again.');
-        } else if(e.message.includes('400') || e.message.includes('404')){
-            let errMsg = '[ServiceNow DevOps] Register Sonar Scan Summaries are not Successful. ';
-            let errMsgSuffix = ' Please provide valid inputs.';
-            let responseData = e.response.data;
-            if (responseData && responseData.result && responseData.result.errorMessage) {
-                errMsg = errMsg + responseData.result.errorMessage + errMsgSuffix;
-                core.setFailed(errMsg);
-            }
-            else if (responseData && responseData.result && responseData.result.details && responseData.result.details.errors) {
-                let errors = responseData.result.details.errors;
-                for (var index in errors) {
-                    errMsg = errMsg + errors[index].message + errMsgSuffix;
-                }
-                core.setFailed(errMsg);
-            }
+            core.setFailed('Invalid Credentials. Please correct the credentials and try again.');
         } else {
-            core.setFailed(`ServiceNow DevOps Event to register Sonar Scan Summaries is not created. Please check ServiceNow logs for more details.`);
+            core.setFailed('ServiceNow Software Quality Results are NOT created. Please check ServiceNow logs for more details.');
         }
     }
-    
-    
+
 })();
